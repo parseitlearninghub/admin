899dfcad1d78290ae79323cfd47728dbdc6a7625
John Lyndo Anuada <parseitlearninghub@gmail.com>
2024-11-26 14:33:38 +0800

    11/26/2024

--- homepage.js
+++ homepage.js
@@ -1,5 +1,16 @@
-function signout() {
-    localStorage.removeItem("user-parser-admin");
-    window.location.href = "login.html";
+// function signout() {
+//     localStorage.removeItem("user-parser-admin");
+//     window.location.href = "login.html";
+// }
+// document.getElementById("username").innerText = localStorage.getItem("user-parser-admin");
+
+window.addEventListener("load", function () {
+    setScreenSize(window.innerWidth, window.innerHeight);
+});
+
+
+//functions
+function setScreenSize(width, height) {
+    document.body.style.width = width + "px";
+    document.body.style.height = height + "px";
 }
-document.getElementById("username").innerText = localStorage.getItem("user-parser-admin");
