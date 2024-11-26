2b7361b22c76b3e9a273f3594264e7842ecb3b03
John Lyndo Anuada <parseitlearninghub@gmail.com>
2024-11-26 14:53:22 +0800

    update

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const parser_uid = localStorage.getItem("user-parser");
+const parser_uid = localStorage.getItem("user-parser-admin");
 if (parser_uid == null) {
     window.location.href = "login.html";
 } else {
