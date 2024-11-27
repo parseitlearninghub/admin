341812633d136673b6fc308f9318d2dfaa889c9d
John Lyndo Anuada <parseitlearninghub@gmail.com>
2024-11-27 08:40:34 +0800

    up

--- homepage.js
+++ homepage.js
@@ -1,4 +1,10 @@
 
+
+//variables
+let year = "";
+let section = "";
+let regular = "";
+
 window.addEventListener("load", function () {
     setScreenSize(window.innerWidth, window.innerHeight);
     document.body.style.display = "flex";
@@ -19,16 +25,29 @@ document.getElementById("canceladdstudent_btn").addEventListener("click", functi
 });
 
 document.getElementById("first_radio").addEventListener("click", function () {
+    year = "1";
     showSection(1)
 });
 document.getElementById("second_radio").addEventListener("click", function () {
+    year = "2";
     showSection(2)
 });
 document.getElementById("third_radio").addEventListener("click", function () {
+    year = "3";
     showSection(3)
 });
 document.getElementById("fourth_radio").addEventListener("click", function () {
+    year = "4";
     showSection(4)
+});
+
+document.getElementById("section-radio-1").addEventListener("click", function () {
+    section = document.getElementById("section-1").getAttribute('data-value');
+    console.log(section);
+});
+document.getElementById("section-radio-2").addEventListener("click", function () {
+    section = document.getElementById("section-2").getAttribute('data-value');
+    console.log(section);
 });
 
 
@@ -41,30 +60,45 @@ function setScreenSize(width, height) {
 function showSection(year) {
     let section_a = "";
     let section_b = "";
+    let value_a = "";
+    let value_b = "";
     if (year === 1) {
         section_a = "1A - Microsoft";
+        value_a = "1A-Microsoft"
         section_b = "1B - Google";
+        value_b = "1B-Google"
     }
 
     if (year === 2) {
         section_a = "2A - Section B";
+        value_a = "2A-SectionA"
         section_b = "2B - Section A";
+        value_b = "2B-SectionB"
 
     }
 
     if (year === 3) {
         section_a = "3A - Section A";
+        value_a = "3A-SectionA"
         section_b = "3B - Section B";
+        value_b = "3B-SectionB"
     }
 
     if (year === 4) {
         section_a = "4A - Xamarin";
+        value_a = "4A-Xamarin"
         section_b = "4B - Laravel";
+        value_b = "4B-Laravel"
 
     }
-
+    document.getElementById('section-radio-1').checked = false;
+    document.getElementById('section-radio-2').checked = false;
     document.getElementById("section-1").innerText = section_a;
+    document.getElementById("section-1").setAttribute('data-value', value_a);
     document.getElementById("section-2").innerText = section_b;
+    document.getElementById("section-2").setAttribute('data-value', value_b);
     document.getElementById("input-section").style.display = "flex";
+
+    //console.log(value_a, value_b, year);
 
 }
