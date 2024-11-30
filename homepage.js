ba7ee43a84553cd7387830a7336795f5ec97f749
John Lyndo Anuada <parseitlearninghub@gmail.com>
2024-11-30 18:18:44 +0800

    Update homepage.js

--- homepage.js
+++ homepage.js
@@ -53,8 +53,8 @@ let admin_id = localStorage.getItem("user-parser-admin");
 
 
 
+setScreenSize(window.innerWidth, window.innerHeight);
 window.addEventListener("load", function () {
-    setScreenSize(window.innerWidth, window.innerHeight);
     document.getElementById("loading_animation_div").style.display = "none";
     document.getElementById("home_div").style.display = "flex";
     subjectparseclass_id = "";
